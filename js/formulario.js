6f2c25ddddfdaa7bef548c3f3601cd2447b73574
joseluis2817 <joseluis2817@hotmail.es>
2015-07-11 11:19:33 -0500

    Revert "Revert "Tecnostylo landing page""
    This reverts commit 72b66e596dcf8c62d8492f98bf83c7e46b0fdd2a.

---
+++ js/formulario.js
@@ -0,0 +1,5 @@
+function mostrariframe(){
+
+document.getElementById("form1").style.display ="block";
+}
+
---
+++ js/fullscreen.js
@@ -0,0 +1,32 @@
+var element = document.getElementById('element');
+var fullscreen = document.getElementById('fullscreen');
+
+
+
+fullscreen.addEventListener('click', function(){
+ if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+
+	});
+
+
+function fullscreen2() {
+
+	 if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
